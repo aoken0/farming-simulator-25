1513e885b890863c762d0292dbb89c297755a6f2
kenaono <aoken.contact@gmail.com>
2025-07-07 21:31:18 +0900

    逆引きを実装中

--- src/app/reverse-production/page.tsx
+++ src/app/reverse-production/page.tsx
@@ -1,23 +1,37 @@
 "use client";
+import { useState, useEffect } from 'react';
 import ContentTitle from "@/components/ContentTitle";
 import GlobalWrapper from "@/components/GlobalWrapper"
 import MenuTable from "@/components/MenuTable";
+import MenuTableTr from "@/components/MenuTableTr";
+import MenuTableTrSelect from "@/components/MenuTableTrSelect";
+import productDataJSON from '../../../public/data/product.json';
+import factoryDataJSON from '../../../public/data/factory.json';
+import { Product, Factory } from '@/utils/type';
 
 const ReverseProduction = () => {
+  const [productData, setProductData] = useState<Product[]>([]);
+  const [factoryData, setFactoryData] = useState<Factory[]>([]);
+
+  useEffect(() => {
+    console.log(factoryDataJSON);
+    console.log(productDataJSON)
+  },[])
+
+  const handleChangeProduct = (production: string) => {
+    console.log(production)
+  }
+
   return (
     <GlobalWrapper>
       <ContentTitle>材料逆引きツール</ContentTitle>
       <MenuTable>
-        <tr>
-            <th>難易度</th>
-            <td>
-              <select name="difficulty" id="difficulty" defaultValue={"normal"}>
-                <option value="easy">イージー</option>
-                <option value="normal">ノーマル</option>
-                <option value="hard">ハード</option>
-              </select>
-            </td>
-          </tr>
+        <MenuTableTr $label="生産品">
+          <MenuTableTrSelect $name="product" $defaultValue="" $onChange={(e) => handleChangeProduct(e.target.value)}>
+            <option value="パン">パン</option>
+            <option value="ケーキ">ケーキ</option>
+          </MenuTableTrSelect>
+        </MenuTableTr>
       </MenuTable>
     </GlobalWrapper>
   )
