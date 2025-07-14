38da9c7f6c4eb8aa0a4be3cedca9682ea216c052
kenaono <aoken.contact@gmail.com>
2025-07-14 21:13:04 +0900

    base リンクの修正

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/farming-simulator-25',
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
--- src/components/GlobalWrapper.tsx
+++ src/components/GlobalWrapper.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import Link from "next/link"
 import { notoSansJP } from "@/utils/font"
 
 type Props = {
@@ -9,15 +10,15 @@ const GlobalWrapper: React.FC<Props> = ({children}) => {
   return (
     <Wrapper className={notoSansJP.className}>
       <Header>
-        <h1><a href="./">Farming Simulator 25 wiki</a></h1>
+        <h1><Link href="/">Farming Simulator 25 wiki</Link></h1>
       </Header>
       <ContentWrapper>
         <SideCotent>
           <h4>シミュレーター</h4>
-          <p><a href="./production-planner">生産計画ツール</a></p>
-          <p><a href="./reverse-production">材料逆引きツール</a></p>
+          <p><Link href="/production-planner">生産計画ツール</Link></p>
+          <p><Link href="/reverse-production">材料逆引きツール</Link></p>
           <h4>価格</h4>
-          <p><a href="./selling-price">売値</a></p>
+          <p><Link href="/selling-price">売値</Link></p>
         </SideCotent>
         <MainContent>
           {children}
