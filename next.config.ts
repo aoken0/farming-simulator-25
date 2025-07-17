95a4369e1abccd2d444695e61566d42f4eaaae3f
kenaono <aoken.contact@gmail.com>
2025-07-17 13:38:17 +0900

    静的サイト用の設定を削除

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  basePath: '/farming-simulator-25',
-  assetPrefix: '/farming-simulator-25',
   trailingSlash: true,
   reactStrictMode: true,
   compiler: {
